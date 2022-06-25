bf74731abacb718c513ee69aa9644efbd1037168
Balazs Venyige <balazs.venyige@doclerholding.com>
2022-06-25 17:24:02 +0200

    "For organizers" page implemented

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,19 @@
 import styles from './Footer.module.scss';
 import SocialMediaIconContainer from './SocialMediaIconContainer/SocialMediaIconContainer';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 export default function Footer() {
+
+	const router = useRouter();
+
 	return (
 		<footer className={styles.container}>
 			<div className={styles.upperDivider}/>
-			<SocialMediaIconContainer />
+			{router.asPath !== '/szervezoknek' &&
+				<Link href="/szervezoknek"><a href="/szervezoknek">Szervezőknek 👈</a></Link>
+			}
+			<SocialMediaIconContainer/>
 			<small className={styles.copyright}>Copyright 2022 @ A Király Halott</small>
 		</footer>
 	);
