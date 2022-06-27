8a544dad6f5271d66d68b2c0f5cb4f57db12ca5e
Balazs Venyige <balazs.venyige@doclerholding.com>
2022-06-27 12:57:55 +0200

    Open Graph meta data added to global page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,28 @@ function MyApp({Component, pageProps}: AppProps) {
 	return (
 		<>
 			<Head>
-				<meta charSet="UTF-8"/>
+				<meta
+					charSet="UTF-8"
+				/>
 				<meta
 					name="keywords"
-			  		content="zene, rock, pop, pop-rock, magyar, király, halott, a király halott, halott király"
+					content="zene, rock, pop, pop-rock, magyar, király, halott, a király halott, halott király"
+				/>
+				<meta
+					property="og:title"
+					content="A Király Halott hivatalos honlapja"
+				/>
+				<meta
+					property="og:description"
+					content="Ismerd meg a zenekart, hallgass bele a zenéjükbe és nézd meg hol találkozhatsz velük legközelebb!"
+				/>
+				<meta
+					property="og:image"
+					content="/akh_og_image.png"
+				/>
+				<meta
+					property="og:type"
+					content="website"
 				/>
 			</Head>
 			<Component {...pageProps} />
