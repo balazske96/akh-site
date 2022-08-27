79b23307fa8855233becc0d75c03f9a206c2506d
Balázs Venyige <venyigebalazs96@gmail.com>
2022-08-27 17:16:59 +0200

    SEO
    google site verification meta tag added to the site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/globals.scss';
-import type {AppProps} from 'next/app';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
 				<meta
-					charSet="UTF-8"
+					name="google-site-verification"
+					content="xc80MrYw-bwwwlvwZrRjxFYk-uejhKkVvSxwb00AYxA"
 				/>
+				<meta charSet="UTF-8" />
 				<meta
 					name="keywords"
 					content="zene, rock, pop, pop-rock, magyar, király, halott, a király halott, halott király"
@@ -21,14 +23,8 @@ function MyApp({Component, pageProps}: AppProps) {
 					property="og:description"
 					content="Ismerd meg a zenekart, hallgass bele a zenéjükbe és nézd meg hol találkozhatsz velük legközelebb!"
 				/>
-				<meta
-					property="og:image"
-					content="/akh_og_image.png"
-				/>
-				<meta
-					property="og:type"
-					content="website"
-				/>
+				<meta property="og:image" content="/akh_og_image.png" />
+				<meta property="og:type" content="website" />
 			</Head>
 			<Component {...pageProps} />
 		</>
