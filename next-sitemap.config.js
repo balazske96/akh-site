94f8ae108c115aa24bd584983572f2b3a6240f15
Balázs Venyige <venyigebalazs96@gmail.com>
2022-08-27 17:08:35 +0200

    sitemap
    custom sitemap builder function implemented

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,29 @@
+function calculatePriority(path){
+	const defaultPriority = 0.7;
+
+	if (path === '/') return 1.0;
+	if (path === '/koncertek') return 0.9;
+	if (path === '/zene') return 0.9;
+	if (path === '/bio') return 0.8;
+
+	return defaultPriority;
+};
+
+
 module.exports = {
 	siteUrl: process.env.SITE_URL || 'https:/akiralyhalott.hu',
 	generateRobotsTxt: true, 
+	exclude: ['/szervezoknek'],
+	transform: async (config, path) => {
+		const priority = calculatePriority(path);
+    
+		// Use default transformation for all other cases
+		return {
+			loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+			changefreq: config.changefreq,
+			priority: priority,
+			lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+			alternateRefs: config.alternateRefs ?? [],
+		};
+	},
 };
