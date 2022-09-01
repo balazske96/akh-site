065b65c3c4c634a414bbf6e01fc84065669d19fa
Balázs Venyige <venyigebalazs96@gmail.com>
2022-09-01 12:05:28 +0200

    rider protection
    Rider protected with password

--- pages/szervezoknek.tsx
+++ pages/szervezoknek.tsx
@@ -1,8 +1,13 @@
 import Layout from '../components/Layout/Layout';
 import styles from '../styles/ForOrganiers.module.scss';
 import Head from 'next/head';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
-export default function ForOrganizers() {
+interface ForOrganiersProps {
+	rider: boolean;
+}
+
+export default function ForOrganizers({ rider }: ForOrganiersProps) {
 	return (
 		<Layout>
 			<Head>
@@ -15,30 +20,65 @@ export default function ForOrganizers() {
 						<h2>Koncertszervezés:</h2>
 						<p>Szűcs Mihály</p>
 						<p>
-							Email: <a href="mailto:szm@koncertszervezo.hu">szm@koncertszervezo.hu</a>
+							Email:{' '}
+							<a href="mailto:szm@koncertszervezo.hu">
+								szm@koncertszervezo.hu
+							</a>
 						</p>
-						<p>Tel: <a href="tel:+36209105312">+36209105312</a></p>
+						<p>
+							Tel: <a href="tel:+36209105312">+36209105312</a>
+						</p>
 					</div>
 					<div className={styles.contactDetails}>
 						<h2>Turné menedzsment:</h2>
 						<p>Berényi Kitti</p>
 						<p>
-							Email: <a href="mailto:info@akiralyhalott.hu">info@akiralyhalott.hu</a>
+							Email:{' '}
+							<a href="mailto:info@akiralyhalott.hu">
+								info@akiralyhalott.hu
+							</a>
 						</p>
-						<p>Tel: <a href="tel:+36704504436">+36704504436</a></p>
+						<p>
+							Tel: <a href="tel:+36704504436">+36704504436</a>
+						</p>
 					</div>
 				</div>
-				<div className={styles.resources}>
-					<ul>
-						<li>
-							<a href="/akh_technikai_rider_2022-06-02.pdf" target="_blank">Technikai Rider 🛠</a>
-						</li>
-						<li>
-							<a href="/akh_catering_rider_2022-06-02.pdf" target="_blank">Catering Rider 🥪</a>
-						</li>
-					</ul>
-				</div>
+				{rider && (
+					<div className={styles.resources}>
+						<ul>
+							<li>
+								<a
+									href="/akh_technikai_rider_2022-06-02.pdf"
+									target="_blank"
+								>
+									Technikai Rider 🛠
+								</a>
+							</li>
+							<li>
+								<a
+									href="/akh_catering_rider_2022-06-02.pdf"
+									target="_blank"
+								>
+									Catering Rider 🥪
+								</a>
+							</li>
+						</ul>
+					</div>
+				)}
 			</div>
 		</Layout>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+	context: GetServerSidePropsContext
+) => {
+	const riderPassword = process.env.RIDER_PASSWORD;
+	const riderPasswordInQuery = (context.query.p as string) ?? '';
+
+	return {
+		props: {
+			rider: riderPassword === riderPasswordInQuery,
+		},
+	};
+};
