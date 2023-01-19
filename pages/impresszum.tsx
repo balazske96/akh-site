b7083beb9b22ae40500933835fc0e1ac1d1091e5
Balázs Venyige <venyigebalazs96@gmail.com>
2023-01-19 19:43:11 +0100

    styling fixes

--- pages/impresszum.tsx
+++ pages/impresszum.tsx
@@ -6,64 +6,57 @@ export default function ImpressumPage(): React.ReactElement {
 		<Layout>
 			<main className={styles.container}>
 				<h1>Impresszum</h1>
-				<ul>
-					<li>
-						<strong>
-							<i>Cégnév:</i>
-						</strong>{' '}
-						Venyige Balázs EV
-					</li>
-					<li>
-						<strong>
-							<i>Telefonszám:</i>
-						</strong>{' '}
-						+36706017860
-					</li>
-					<li>
-						<strong>
-							<i>E-mail:</i>
-						</strong>{' '}
-						venyigebalazs96@gmail.com
-					</li>
-					<li>
-						<strong>
-							<i>A céget bejegyző hatóság:</i>
-						</strong>{' '}
-						Nemzeti Adó- és Vámhivatal
-					</li>
-					<li>
-						<strong>
-							<i>Adószám:</i>
-						</strong>{' '}
-						59797920-1-42
-					</li>
-					<li>
-						<strong>
-							<i>Tárhelyszolgáltató:</i>
-						</strong>{' '}
-						Vercel Inc.
-						<br />
-						340 S Lemon Ave #4133
-						<br />
-						Walnut, CA 91789
-						<br />
-						<a
-							href="https://vercel.com/"
-							target={'_blank'}
-							rel="noreferrer"
-						>
-							vercel.com
-						</a>
-						<br />
-						<a
-							href="mailto:privacy@vercel.com"
-							target={'_blank'}
-							rel="noreferrer"
-						>
-							privacy@vercel.com
-						</a>
-					</li>
-				</ul>
+				<span>
+					<strong>
+						<i>Cégnév:</i>
+					</strong>{' '}
+					Venyige Balázs EV
+				</span>
+				<span>
+					<strong>
+						<i>E-mail:</i>
+					</strong>{' '}
+					venyigebalazs96@gmail.com
+				</span>
+				<span>
+					<strong>
+						<i>A céget bejegyző hatóság:</i>
+					</strong>{' '}
+					Nemzeti Adó- és Vámhivatal
+				</span>
+				<span>
+					<strong>
+						<i>Adószám:</i>
+					</strong>{' '}
+					59797920-1-42
+				</span>
+				<span>
+					<strong>
+						<i>Tárhelyszolgáltató:</i>
+					</strong>
+					<br />
+					Vercel Inc.
+					<br />
+					340 S Lemon Ave #4133
+					<br />
+					Walnut, CA 91789
+					<br />
+					<a
+						href="https://vercel.com/"
+						target={'_blank'}
+						rel="noreferrer"
+					>
+						vercel.com
+					</a>
+					<br />
+					<a
+						href="mailto:privacy@vercel.com"
+						target={'_blank'}
+						rel="noreferrer"
+					>
+						privacy@vercel.com
+					</a>
+				</span>
 			</main>
 		</Layout>
 	);
