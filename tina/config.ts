47d09339b49e6f21dc6a4d6f219fcd54a1481a46
Balázs Venyige <venyigebalazs96@gmail.com>
2023-04-08 20:01:07 +0200

    Tina FIX v5

--- tina/config.ts
+++ tina/config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tinacms';
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-	process.env.HEAD || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || 'main';
+const branch = 'new-cms';
+// process.env.HEAD || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || 'main';
 
 export default defineConfig({
 	branch,
